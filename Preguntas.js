826051a42b1e10b7bd7934934bd28191625a9fe1
DagonGamer <davidgonzalezleon21@gmail.com>
2022-09-02 13:01:53 +0200

    Dagole Web

---
+++ Preguntas.js
@@ -0,0 +1,172 @@
+let RimasCho = [
+    "Comeme el cho***",
+    "Por el cu** te la entrocho",
+];
+
+let RimasTres = [
+    "Te la meto del revés",
+    "Te la mete el marqués",
+];
+
+let RimasEte = [
+    "Te la meto en el retrete",
+    "Tu pe** miente",
+    "Por la boca te la mete",
+    "En la cama eres un guarrete"
+];
+
+let RimasEis = [
+    "Si os agachais me la veis",
+    "Me la cogeis"
+];
+
+let RimasDiez = [
+    "La metes en la nuez",
+    "Chupamela otra vez"
+];
+
+let RimasOnce = [
+    "La tengo de bronce",
+    "Te la mete el conde"
+];
+
+let RimasDoce = [
+    "Te la meto sin que roce",
+    "Te la meto en el monte"
+];
+
+var Preguntas = {
+
+    // Saludos
+
+    "Hola": "Hola buenos días jajaja, aunque no sea de día, me da igual, aún de noche sigue pudiendose decir buenos días jajajajja",
+    "Buenos dias": "Buenos días por la mañana temprano jajajajaj",
+
+    // Más cosas
+
+    "Preguntame": "Eres feliz?",
+
+    // Preguntas
+
+    "Que hay de comer?": "Buena pregunta, yo comeré comida virtual supongo, archivos HTML o algo así, pero tu no creo que puedas jajajajaja",
+    "Como te llamas?": "Yo no me llamo, normalmente alguien me llama, por lo que la pregunta correcta sería 'Como te llaman?' jajaj",
+    "Como te llaman?": "Dagole, el bot de la cuenta de instagram jaja",
+    "Para que existes?": "Para estar aquí, en la página Normal de Dagole",
+    "Por qué existes?": "Porque patata, o jamón york, yo que se, simplemente existo jaja",
+    "Hay clase hoy?": "Mañana no hay clase mañana no hay clase mañana no hay clase... jajajajjajajja",
+    "Sabes rimar?": "Si se, con los números, pero soy un poco grosero jaja",
+    "Eres feliz?": "Si jaja",
+    "Eres tonto?": "Solo me lo hago jajaj",
+    "Cuanto me quieres?": "Pos según quién seas te quiero entre nada y mucho, pero mucho de muchísimo, más o menos entre esas cantidades están la mayoría de personas, y tu estarás también supongo jaja",
+    
+    // Preguntas de Te gusta...?
+    
+    "Te gusta reir?": "Mucho, demasiado diria incluso jajaj",
+    "Te gustan grandes?": "Si, mucho, las cartas grandes se ven mejor jajajaj, así se puede hechar una buena brisca jjaja o un chinchón jajajaj",
+    "Te gusta el pan?": "Si jajajajja, está muy bueno la verdad",
+    "Te gusta escribir?": "Puff, poco pero... No, no me gusta jajajaj",
+    
+    // Rimas de números
+
+    "Uno": "Con mi pe** te vacuno",
+    "Dos": "Te la meto como dios",
+    "Tres": RimasTres,
+    "Cuatro": "Por tu cu** mi aparato",
+    "Cinco": "Por el cu** te la inco",
+    "Seis": RimasEis,
+    "Siete": RimasEte,
+    "Ocho": RimasCho,
+    "Nueve": "En tu cu** se me mueve",
+    "Diez": RimasDiez,
+    "Once": RimasOnce,
+    "Doce": RimasDoce,
+    "Trece": "En tu cu** se me cuece",
+    "Catorce": "Por el cu** te la entrocho",
+    "Quince": "No me hagas un esguince",
+    "Dieciseis": RimasEis,
+    "Diecisiete": RimasEte,
+    "Dieciocho": RimasCho,
+    "Diecinueve": "En tu cu** se me mueve",
+    "Veinte": "Me pe** hasta en tu mente",
+    "Veintiuno": "Con mi pe** te vacuno",
+    "Veintidos": "Te la meto como dios",
+    "Veintitres": RimasTres,
+    "Veinticuatro": "Por tu cu** mi aparato",
+    "Veinticinco": "Por el cu** te la inco",
+    "Veintiseis": RimasEis,
+    "Veintisiete": RimasEte,
+    "Veintiocho": RimasCho,
+    "Veintinueve": "En tu cu** se me mueve",
+    "Treinta": "Chupamela con menta",
+    "Treinta y uno": "Con mi pe** te vacuno",
+    "Treinta y dos": "Te la meto como dios",
+    "Treinta y tres": RimasTres,
+    "Treinta y cuatro": "Por tu cu** mi aparato",
+    "Treinta y cinco": "Por el cu** te la inco",
+    "Treinta y seis": RimasEis,
+    "Treinta y siete": RimasEte,
+    "Treinta y ocho": RimasCho,
+    "Treinta y nueve": "En tu cu** se me mueve",
+    "Cuarenta": "Chupamela con menta",
+    "Cuarenta y uno": "Con mi pe** te vacuno",
+    "Cuarenta y dos": "Te la meto como dios",
+    "Cuarenta y tres": RimasTres,
+    "Cuarenta y cuatro": "Por tu cu** mi aparato",
+    "Cuarenta y cinco": "Por el cu** te la inco",
+    "Cuarenta y seis": RimasEis,
+    "Cuarenta y siete": RimasEte,
+    "Cuarenta y ocho": RimasCho,
+    "Cuarenta y nueve": "En tu cu** se me mueve",
+    "Cincuenta": "Chupamela con menta",
+    "Cincuenta y uno": "Con mi pe** te vacuno",
+    "Cincuenta y dos": "Te la meto como dios",
+    "Cincuenta y tres": RimasTres,
+    "Cincuenta y cuatro": "Por tu cu** mi aparato",
+    "Cincuenta y cinco": "Por el cu** te la inco",
+    "Cincuenta y seis": RimasEis,
+    "Cincuenta y siete": RimasEte,
+    "Cincuenta y ocho": RimasCho,
+    "Cincuenta y nueve": "En tu cu** se me mueve",
+    "Sesenta": "Chupamela con menta",
+    "Sesenta y uno": "Con mi pe** te vacuno",
+    "Sesenta y dos": "Te la meto como dios",
+    "Sesenta y tres": RimasTres,
+    "Sesenta y cuatro": "Por tu cu** mi aparato",
+    "Sesenta y cinco": "Por el cu** te la inco",
+    "Sesenta y seis": RimasEis,
+    "Sesenta y siete": RimasEte,
+    "Sesenta y ocho": RimasCho,
+    "Sesenta y nueve": "En tu cu** se me mueve",
+    "Setenta": "Chupamela con menta",
+    "Setenta y uno": "Con mi pe** te vacuno",
+    "Setenta y dos": "Te la meto como dios",
+    "Setenta y tres": RimasTres,
+    "Setenta y cuatro": "Por tu cu** mi aparato",
+    "Setenta y cinco": "Por el cu** te la inco",
+    "Setenta y seis": RimasEis,
+    "Setenta y siete": RimasEte,
+    "Setenta y ocho": RimasCho,
+    "Setenta y nueve": "En tu cu** se me mueve",
+    "Ochenta": "Chupamela con menta",
+    "Ochenta y uno": "Con mi pe** te vacuno",
+    "Ochenta y dos": "Te la meto como dios",
+    "Ochenta y tres": RimasTres,
+    "Ochenta y cuatro": "Por tu cu** mi aparato",
+    "Ochenta y cinco": "Por el cu** te la inco",
+    "Ochenta y seis": RimasEis,
+    "Ochenta y siete": RimasEte,
+    "Ochenta y ocho": RimasCho,
+    "Ochenta y nueve": "En tu cu** se me mueve",
+    "Noventa": "Chupamela con menta",
+    "Noventa y uno": "Con mi pe** te vacuno",
+    "Noventa y dos": "Te la meto como dios",
+    "Noventa y tres": RimasTres,
+    "Noventa y cuatro": "Por tu cu** mi aparato",
+    "Noventa y cinco": "Por el cu** te la inco",
+    "Noventa y seis": RimasEis,
+    "Noventa y siete": RimasEte,
+    "Noventa y ocho": RimasCho,
+    "Noventa y nueve": "En tu cu** se me mueve",
+    "Cien": "Miramela y ven"
+
+}
